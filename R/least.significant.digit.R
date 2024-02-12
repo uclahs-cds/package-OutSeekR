30d9f2300b0cd4335fb7ae9c6b0fecd1d8a631e6
John M Sahrmann <jsahrmann@mednet.ucla.edu>
2024-02-12 09:38:41 -0800

    Add script `R/least.significant.digit.R`

---
+++ R/least.significant.digit.R
@@ -0,0 +1,26 @@
+#' Least significant digit
+#'
+#' Return the least significant digit from a vector of numbers.
+#'
+#' @param x A numeric vector.
+#' @return The least significant digit among the numbers in `x`, with the digit itself replaced by 1.  See Examples.
+#'
+#' @examples
+#' least.significant.digit(
+#'     x = c(0.2, 0.03, 0.004, 0.05)
+#'     );
+least.significant.digit <- function(x) {
+    decimal.number.max <- sapply(
+        X = na.omit(x),
+        FUN = function(y) {
+            decimal.numbers <- sapply(
+                X = y,
+                FUN = function(z) {
+                    nchar(as.character(z)) - nchar(as.integer(z)) - 1
+                    }
+                );
+            decimal.numbers;
+            }
+        );
+    1 / 10^as.numeric(max(decimal.number.max));
+    }
