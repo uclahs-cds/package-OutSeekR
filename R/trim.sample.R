5cc1d94a2c169f2864e87364ec75943315a3e765
John M Sahrmann <jsahrmann@mednet.ucla.edu>
2024-02-12 09:36:32 -0800

    Add script `R/trim.sample.R`

---
+++ R/trim.sample.R
@@ -0,0 +1,26 @@
+#' Trim a vector of numbers
+#'
+#' Symmetrically trim a vector of numbers after sorting it.
+#'
+#' @details If `length(x) <= 10`, the function returns `x[2:(length(x) - 1)]`.
+#'
+#' @param x A numeric vector.
+#' @param trim A number, the fraction of observations to be trimmed from each end of `x`.
+#' @return A sorted, trimmed copy of `x`.
+#'
+#' @examples
+#' trim.sample(
+#'     x = 1:20,
+#'     trim = 0.05
+#'     );
+trim.sample <- function(x, trim = 0.05) {
+    x <- sort(x);
+    if (length(x) <= 10) {
+        patient.trim.value <- 2:(length(x) - 1);
+        } else {
+        trim.sample.number <- length(x) * trim;
+        trim.sample.number.integer <- round(trim.sample.number);
+        patient.trim.value <- (trim.sample.number.integer + 1):(length(x) - trim.sample.number.integer);
+        }
+    x[patient.trim.value];
+    }
