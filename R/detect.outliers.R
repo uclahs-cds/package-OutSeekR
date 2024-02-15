00945f42fe5b8521900fafb81c640db19a6e9e88
John M Sahrmann <jsahrmann@mednet.ucla.edu>
2024-02-15 14:34:55 -0800

    Create five method data set from observed data
    Create data set of the five outlier detection statistics for each
    transcript in the observed (i.e., input) data in `detect.outliers()`:
    
    - add calls to `outlier.detection.zrange()` and
      `outlier.detection.kmeans()`;
    - compute cosine similarity for each transcript;
    - transpose the intermediate data sets so that the arrangement of rows
      and columns matches that of the input data; and
    - bind the zranges, k-means fraction, and cosine similarity for each
      transcript into a data set.

--- R/detect.outliers.R
+++ R/detect.outliers.R
@@ -24,18 +24,18 @@ detect.outliers <- function(data) {
         FUN = quantify.outliers,
         method = 'mean'
         );
+    data.median <- future.apply::future_apply(
+        X = data,
+        MARGIN = 1,
+        FUN = quantify.outliers,
+        method = 'median'
+        );
     data.trimmean <- future.apply::future_apply(
         X = data,
         MARGIN = 1,
         FUN = quantify.outliers,
         method = 'mean',
         trim = 0.05
-        );
-    data.median <- future.apply::future_apply(
-        X = data,
-        MARGIN = 1,
-        FUN = quantify.outliers,
-        method = 'median'
         );
     data.kmeans <- future.apply::future_apply(
         X = data,
@@ -45,12 +45,72 @@ detect.outliers <- function(data) {
         nstart = 1000,
         future.seed = TRUE
         );
+    # Transpose so the rows and columns in the computed quantities
+    # match those in the original data.
+    data.mean <- t(data.mean);
+    data.median <- t(data.median);
+    data.trimmean <- t(data.trimmean);
+    data.kmeans <- t(data.kmeans);
+    # Compute the ranges of the z-score statistics.
+    data.zrange.mean <- future.apply::future_apply(
+        X = data.mean,
+        MARGIN = 1,
+        FUN = outlier.detection.zrange
+        );
+    data.zrange.median <- future.apply::future_apply(
+        X = data.median,
+        MARGIN = 1,
+        FUN = outlier.detection.zrange
+        );
+    data.zrange.trimmean <- future.apply::future_apply(
+        X = data.trimmean,
+        MARGIN = 1,
+        FUN = outlier.detection.zrange
+        );
+    # Compute the k-means fraction.
+    data.fraction.kmeans <- future.apply::future_apply(
+        X = data.kmeans,
+        MARGIN = 1,
+        FUN = outlier.detection.kmeans
+        );
+    # Transpose so the rows and columns in the computed quantities
+    # match those in the original data.
+    data.zrange.mean <- t(data.zrange.mean);
+    data.zrange.median <- t(data.zrange.median);
+    data.zrange.trimmean <- t(data.zrange.trimmean);
+    data.fraction.kmeans <- t(data.fraction.kmeans);
+    # Compute the cosine similarity.
+    data.cosine <- future.apply::future_sapply(
+        X = seq_len(nrow(data)),
+        FUN = function(i) {
+            outlier.detection.cosine(
+                x = as.numeric(data[i, ]),
+                distribution = optimal.distribution.data[i]
+                );
+            }
+        );
+    names(data.cosine) <- rownames(data);
+    # Assemble the statistics from the five methods into a single
+    # matrix.
+    observed.5method <- cbind(
+        zrange.mean = data.zrange.mean[, 'zrange'],
+        zrange.median = data.zrange.median[, 'zrange'],
+        zrange.trimmean = data.zrange.trimmean[, 'zrange'],
+        fraction.kmeans = data.fraction.kmeans[, 'fraction'],
+        cosine = data.cosine
+        );
 
     list(
         optimal.distribution.data = optimal.distribution.data,
         data.mean = data.mean,
-        data.trimmean = data.trimmean,
         data.median = data.median,
-        data.kmeans = data.kmeans
+        data.trimmean = data.trimmean,
+        data.kmeans = data.kmeans,
+        data.zrange.mean = data.zrange.mean,
+        data.zrange.median = data.zrange.median,
+        data.zrange.trimmean = data.zrange.trimmean,
+        data.fraction.kmeans = data.fraction.kmeans,
+        data.cosine = data.cosine,
+        observed.5method = observed.5method
         );
     }
