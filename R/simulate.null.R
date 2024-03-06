42fbd358c7f0b9fff765022f67b5c4ace322c926
John M Sahrmann <jsahrmann@mednet.ucla.edu>
2024-03-06 10:01:29 -0800

    Draft alternative simulation function
    Draft an alternative function definition, tentatively called
    `simulate.null2()`, for simulating null transcripts.  This function
    combines the computations from the original `simulate.null()` function
    with those of the new `add.residual.noise()` function, with the
    benefit being that we ultimately end up with one `num.null` by
    `ncol(data)` matrix rather than two, thereby relieving memory
    constraints.

--- R/simulate.null.R
+++ R/simulate.null.R
@@ -144,3 +144,122 @@ simulate.null <- function(
     rownames(simulated.null) <- rownames(data)[sampled.indices];
     simulated.null;
     }
+
+
+simulate.null2 <- function(
+    x,
+    x.distribution,
+    r,
+    r.distribution
+    ) {
+    #
+    # Simulate transcripts
+    #
+    # Ensure the values in `x` are strictly positive.
+    add.minimum.value <- least.significant.digit(x);
+    x.nozero <- x + add.minimum.value;
+    # Apply 5% trimming.
+    x.trim <- trim.sample(x);
+    x.nozero.trim <- trim.sample(x.trim);
+    # Generate null values according to the optimal
+    # distribution for this transcript.
+    if (1 == x.distribution) {
+        norm.mean <- mean(x.nozero.trim);
+        norm.sd <- stats::sd(x.trim);
+        simulated.null <- truncnorm::rtruncnorm(
+            n = length(x),
+            mean = norm.mean,
+            sd = norm.sd,
+            a = 0
+            );
+        }
+    else if (2 == x.distribution) {
+        mean.log <- mean(x.nozero.trim);
+        sd.log <- stats::sd(x.nozero.trim);
+        m2 <-  log(mean.log^2 / sqrt(sd.log^2 + mean.log^2));
+        sd2 <- sqrt(log(1 + (sd.log^2 / mean.log^2)));
+        simulated.null <- stats::rlnorm(
+            n = length(x),
+            meanlog = m2,
+            sdlog = sd2
+            );
+        }
+    else if (3 == x.distribution) {
+        exp.rate <- 1 / mean(x.nozero.trim);
+        simulated.null <- stats::rexp(
+            n = length(x),
+            rate = exp.rate
+            );
+        }
+    else if (4 == x.distribution) {
+        mean.gamma <- mean(x.nozero.trim);
+        sd.gamma <- stats::sd(x.nozero.trim);
+        gamma.shape <- (mean.gamma/sd.gamma)^2;
+        gamma.rate <- mean.gamma/(sd.gamma^2);
+        simulated.null <- stats::rgamma(
+            n = length(x),
+            shape = gamma.shape,
+            rate = gamma.rate
+            );
+        }
+    #
+    # Simulate noise
+    #
+    # Ensure the values in `r` are strictly positive.
+    add.minimum.value <- least.significant.digit(r)
+    if (min(r) < 0) {
+        r.nozero <- r - min(r) + add.minimum.value;
+        }
+    else {
+        r.nozero <- r + add.minimum.value;
+        }
+    # Generate null values from the distribution coded by
+    # `distribution`.
+    if (1 == r.distribution) {
+        norm.mean <- mean(r.nozero);
+        norm.sd <- stats::sd(r.nozero);
+        simulated.noise <- truncnorm::rtruncnorm(
+            n = length(x),
+            mean = norm.mean,
+            sd = norm.sd,
+            a = 0
+            );
+        }
+    else if (2 == r.distribution) {
+        mean.log <- mean(r.nozero);
+        sd.log <- stats::sd(r.nozero);
+        m2 <- log(mean.log^2 / sqrt(sd.log^2 + mean.log^2));
+        sd2 <- sqrt(log(1 + (sd.log^2 / mean.log^2)));
+        simulated.noise <- stats::rlnorm(
+            n = length(x),
+            meanlog = m2,
+            sdlog = sd2
+            );
+        }
+    else if (3 == r.distribution) {
+        exp.rate <- 1 / mean(r.nozero);
+        simulated.noise <- stats::rexp(
+            n = length(x),
+            rate = exp.rate
+            );
+        }
+    else if (4 == r.distribution) {
+        mean.gamma <- mean(r.nozero);
+        sd.gamma <- stats::sd(r.nozero);
+        gamma.shape <- (mean.gamma / sd.gamma)^2;
+        gamma.rate <- mean.gamma / (sd.gamma^2);
+        simulated.noise <- stats::rgamma(
+            n = length(x),
+            shape = gamma.shape,
+            rate = gamma.rate
+            );
+        }
+    if (min(r) < 0) {
+        simulated.noise <- simulated.noise + min(r) - add.minimum.value;
+        }
+    else {
+        simulated.noise <- simulated.noise - add.minimum.value;
+        }
+    # Add the simulated noise to the simulated transcript.
+    abs(simulated.null + simulated.noise)
+    }
