bff66b4af205736a9328596c0e5a9c3af32280b3
John M Sahrmann <jsahrmann@mednet.ucla.edu>
2024-04-09 10:25:06 -0700

    Remove alternate definitions in R/rank.outliers.R
    Remove alternate definitions of the functions `outlier.rank()` and
    `outlier.rank.product()` in the script `R/rank.outliers.R`.  Update
    documentation and examples accordingly.

--- R/rank.outliers.R
+++ R/rank.outliers.R
@@ -1,15 +1,17 @@
 #' Rank outlier statistics
 #'
-#' Given a matrix of outlier statistics, organized with transcripts on rows and outlier statistics on columns, return the matrix formed by replacing the values of the outlier statistics with their ranks within each column.
+#' Given a vector of outlier statistics and the name of the method used to compute them, return the vector of ranks of the statistics.
 #'
 #' @details Ranks are computed such that smaller ranks correspond to more extreme values of the outlier statistics.  For z-ranges based on the mean and standard deviation, trimmed mean and trimmed standard deviation, and median and median absolute deviation, this requires first multiplying the values of the statistics by -1.
 #'
-#' @param outlier.statistics.matrix A matrix of outlier statistics such as those returned by `quantify.outliers()` and `outlier.detection.cosine()`.
-#' @return A numeric matrix with the same dimensions as the input.  Row and column names are preserved.
+#' @param outlier.statistic A numeric vector of outlier statistics such as that returned by `quantify.outliers()` and `outlier.detection.cosine()`.
+#' @param method A string describing the method by which the values in `outlier.statistic` were computed.  One of `'zrange.mean'`, `'zrange.median'`, `'zrange.trimmean'`, `'fraction.kmeans'`, or `'cosine.similarity'`.
+#'
+#' @return A numeric vector of ranks.  Names are preserved.
 #'
 #' @examples
 #' # Generate example outlier statistics.
-#' x <- matrix(
+#' outlier.stats.matrix <- matrix(
 #'     data = c(
 #'          4.9254,  4.4737,  6.0652,  5.9465,  6.0137,  5.3294,
 #'          6.6455,  4.9609, 11.6272, 18.6988,  4.7265,  5.7300,
@@ -27,43 +29,30 @@
 #'             )
 #'         )
 #'     );
-#' outlier.rank(x);
+#' # Assign ranks within each method.
+#' ranks.zrange.mean <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'zrange.mean'],
+#'     method = 'zrange.mean'
+#'     );
+#' ranks.zrange.median <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'zrange.median'],
+#'     method = 'zrange.median'
+#'     );
+#' ranks.zrange.trimmean <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'zrange.trimmean'],
+#'     method = 'zrange.trimmean'
+#'     );
+#' ranks.fraction.kmeans <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'fraction.kmeans'],
+#'     method = 'fraction.kmeans'
+#'     );
+#' ranks.cosine.similarity <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'cosine.similarity'],
+#'     method = 'cosine.similarity'
+#'     );
 #'
 #' @noRd
-outlier.rank <- function(outlier.statistics.matrix) {
-    methods <- c(
-        'zrange.mean', 'zrange.median', 'zrange.trimmean',
-        'fraction.kmeans', 'cosine.similarity'
-        );
-    ranks.matrix <- matrix(
-        data = NA,
-        nrow = nrow(outlier.statistics.matrix),
-        ncol = ncol(outlier.statistics.matrix),
-        dimnames = list(
-            rownames(outlier.statistics.matrix),
-            colnames(outlier.statistics.matrix)
-            )
-        );
-    for (method in methods) {
-        if (method %in% c('zrange.mean', 'zrange.median', 'zrange.trimmean')) {
-            ranks.matrix[, method] <- rank(
-                x = -outlier.statistics.matrix[, method],
-                na.last = 'keep',
-                ties.method = 'max'
-                );
-            }
-        else if (method %in% c('fraction.kmeans', 'cosine.similarity')) {
-            ranks.matrix[, method] <- rank(
-                x = outlier.statistics.matrix[, method],
-                na.last = 'keep',
-                ties.method = 'max'
-                );
-            }
-        }
-    ranks.matrix;
-    }
-
-outlier.rank2 <- function(outlier.statistic, method) {
+outlier.rank <- function(outlier.statistic, method) {
     if (method %in% c('zrange.mean', 'zrange.median', 'zrange.trimmean')) {
         ranks <- rank(
             x = -outlier.statistic,
@@ -83,18 +72,24 @@ outlier.rank2 <- function(outlier.statistic, method) {
 
 #' Rank product of outlier statistics
 #'
-#' Given a matrix of ranks of outlier statistics, organized with transcripts on rows and outlier statistics on columns, return the products of the ranks across each transcript.
+#' Given ranks for each of the five outlier statistics, return the rank product.
 #'
 #' @details For \eqn{k} nonmissing ranks, the rank product is defined as
 #' \deqn{\sqrt[k]{\prod_{i=1}^k Rank_i}}
 #'
-#' @param ranks.matrix A matrix of ranks of outlier statistics such as that returned by `outlier.rank()`.
+#' @param zrange.mean A numeric vector of ranks from the range of z-scores based on the mean and standard deviation.
+#' @param zrange.median A numeric vector of ranks from the range of z-scores based on the median and median absolute deviation.
+#' @param zrange.trimmean A numeric vector of ranks from the range of z-scores based on the trimmed mean and trimmed standard deviation.
+#' @param fraction.kmeans A numeric vector of ranks from the cluster assignment from k-means with two clusters.
+#' @param cosine.similarity A numeric vector of ranks from the cosine similarity.
 #' @param num.allowed.NA The number of allowable missing values in each row of ranks.  If the number of missing values exceeds `num.allowed.NA`, the corresponding rank product will be `NA`.  Default is zero.
+#'
 #' @return A numeric vector of length equal to `nrow(ranks.matrix)` with names taken from `rownames(ranks.matrix)`.
 #'
 #' @examples
+#'
 #' # Generate example outlier statistics.
-#' x <- matrix(
+#' outlier.stats.matrix <- matrix(
 #'     data = c(
 #'          4.9254,  4.4737,  6.0652,  5.9465,  6.0137,  5.3294,
 #'          6.6455,  4.9609, 11.6272, 18.6988,  4.7265,  5.7300,
@@ -112,13 +107,39 @@ outlier.rank2 <- function(outlier.statistic, method) {
 #'             )
 #'         )
 #'     );
-#' ranks <- outlier.rank(x);
+#' # Assign ranks within each method.
+#' ranks.zrange.mean <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'zrange.mean'],
+#'     method = 'zrange.mean'
+#'     );
+#' ranks.zrange.median <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'zrange.median'],
+#'     method = 'zrange.median'
+#'     );
+#' ranks.zrange.trimmean <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'zrange.trimmean'],
+#'     method = 'zrange.trimmean'
+#'     );
+#' ranks.fraction.kmeans <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'fraction.kmeans'],
+#'     method = 'fraction.kmeans'
+#'     );
+#' ranks.cosine.similarity <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'cosine.similarity'],
+#'     method = 'cosine.similarity'
+#'     );
+#'
 #' outlier.rank.product(
-#'     ranks.matrix = ranks
+#'     zrange.mean = ranks.zrange.mean,
+#'     zrange.median = ranks.zrange.median,
+#'     zrange.trimmean = ranks.zrange.trimmean,
+#'     fraction.kmeans = ranks.fraction.kmeans,
+#'     cosine.similarity = ranks.cosine.similarity,
+#'     num.allowed.NA = 0
 #'     );
 #'
 #' # Allow at most one missing rank.
-#' x <- matrix(
+#' outlier.stats.matrix <- matrix(
 #'     data = c(
 #'          4.9254,  4.4737,  6.0652,  5.9465,  6.0137,  5.3294,
 #'          6.6455,  4.9609, 11.6272, 18.6988,  4.7265,  5.7300,
@@ -136,30 +157,39 @@ outlier.rank2 <- function(outlier.statistic, method) {
 #'             )
 #'         )
 #'     );
-#' ranks <- outlier.rank(x);
+#'
+#' ranks.zrange.mean <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'zrange.mean'],
+#'     method = 'zrange.mean'
+#'     );
+#' ranks.zrange.median <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'zrange.median'],
+#'     method = 'zrange.median'
+#'     );
+#' ranks.zrange.trimmean <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'zrange.trimmean'],
+#'     method = 'zrange.trimmean'
+#'     );
+#' ranks.fraction.kmeans <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'fraction.kmeans'],
+#'     method = 'fraction.kmeans'
+#'     );
+#' ranks.cosine.similarity <- outlier.rank(
+#'     outlier.statistic = outlier.stats.matrix[, 'cosine.similarity'],
+#'     method = 'cosine.similarity'
+#'     );
+#'
 #' outlier.rank.product(
-#'     ranks.matrix = ranks,
+#'     zrange.mean = ranks.zrange.mean,
+#'     zrange.median = ranks.zrange.median,
+#'     zrange.trimmean = ranks.zrange.trimmean,
+#'     fraction.kmeans = ranks.fraction.kmeans,
+#'     cosine.similarity = ranks.cosine.similarity,
 #'     num.allowed.NA = 1
 #'     );
 #'
 #' @noRd
-outlier.rank.product <- function(ranks.matrix, num.allowed.NA = 0) {
-    num.NA <- future.apply::future_apply(
-        X = ranks.matrix,
-        MARGIN = 1,
-        FUN = function(x) sum(is.na(x))
-        );
-    rank.product <- future.apply::future_apply(
-        X = ranks.matrix,
-        MARGIN = 1,
-        FUN = prod,
-        na.rm = TRUE
-        );
-    rank.product[num.NA > num.allowed.NA] <- NA;
-    rank.product^(1 / (ncol(ranks.matrix) - num.NA));
-    }
-
-outlier.rank.product2 <- function(
+outlier.rank.product <- function(
     zrange.mean,
     zrange.median,
     zrange.trimmean,
