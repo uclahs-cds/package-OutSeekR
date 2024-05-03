29308a640bdab4a839d0e494abce034a90294fb0
John M Sahrmann <jsahrmann@mednet.ucla.edu>
2024-05-03 13:57:52 -0700

    Add `calculate.p.values.sans.null.data()`
    Add function `calculate.p.values.sans.null.data()` to script
    `R/calculate.p.values.R`.  This alternate version of
    `calculate.p.values()` uses the precomputed outlier statistics for the
    complete null data set to calculate the p-value for each sample in the
    observed transcript rather than excluding an arbitrarily chosen null
    sample and recomputing the outlier statistics.  Only the outlier
    statistics for the observed transcript are recomputed as each new
    outlier is discovered and excluded.  Because the null data do not
    contain outliers by construction, the choice of what sample to exclude
    was irrelevant, with a further implication being that an exclusion of
    a sample in the null data is altogether unnecessary.  Relying on the
    precomputed outlier statistics for the complete null data means we no
    longer need to pass `null.data` to `calculate.p.values()`, leading to
    a massive improvement in both computation time and memory use.

--- R/calculate.p.values.R
+++ R/calculate.p.values.R
@@ -530,3 +530,215 @@ calculate.p.values2 <- function(
         outlier.statistics.list = outlier.statistics.list
         );
     }
+
+#' Calculate p-values, without `null.data`
+#'
+#' Calculate p-values for a single transcript.
+#'
+#' @return A list consisting of the following entries:
+#' * `p.values`: a vector of p-values for the outlier test run on each sample (up until the p-value exceeds `p.value.threshold`); and
+#' * `outlier.statistics.list`, a list of vectors containing the values of the outlier statistics calculated from the remaining samples.  The list will be of length equal to one plus the total number of outliers (i.e., the number of samples with an outlier test p-value less than `p.value.threshold`) and will contain entries `outlier.statistics.N`, where `N` is between zero and the total number of outliers.  `outlier.statistics.N` is the vector of outlier statistics after excluding the `N`th outlier sample, with `outlier.statistics.0` being for the complete transcript.
+#'
+#' @noRd
+calculate.p.values.sans.null.data <- function(
+    x,
+    x.distribution,
+    x.zrange.mean,
+    x.zrange.median,
+    x.zrange.trimmean,
+    x.fraction.kmeans,
+    x.cosine.similarity,
+    null.zrange.mean,
+    null.zrange.median,
+    null.zrange.trimmean,
+    null.fraction.kmeans,
+    null.cosine.similarity,
+    p.value.threshold = 0.05,
+    kmeans.nstart = 1
+    ) {
+    p.values <- rep(NA, length(x));
+    names(p.values) <- names(x);
+    # Get the name and index of the most abundant sample in `x`.
+    index.most.abundant.sample <- which.max(x);
+    most.abundant.sample <- names(x)[index.most.abundant.sample];
+    # Initialize a list to store the outlier statistics for `x` as
+    # well as subsequent copies of `x` as each outlier is excluded.
+    outlier.statistics.list <- list();
+    # Record the outlier statistics for `x`.
+    outlier.statistics.list[['outlier.statistics.0']] <- c(
+        x.zrange.mean,
+        x.zrange.median,
+        x.zrange.trimmean,
+        x.fraction.kmeans,
+        x.cosine.similarity
+        );
+    names(outlier.statistics.list[['outlier.statistics.0']]) <- c(
+        'zrange.mean',
+        'zrange.median',
+        'zrange.trimmean',
+        'fraction.kmeans',
+        'cosine.similarity'
+        );
+
+    # Combine the outlier statistics from this observed transcript
+    # with those of the null data.
+    zrange.mean <- c(x.zrange.mean, null.zrange.mean);
+    zrange.median <- c(x.zrange.median, null.zrange.median);
+    zrange.trimmean <- c(x.zrange.trimmean, null.zrange.trimmean);
+    fraction.kmeans <- c(x.fraction.kmeans, null.fraction.kmeans);
+    cosine.similarity <- c(x.cosine.similarity, null.cosine.similarity);
+    # Assign ranks within each method.
+    rank.zrange.mean <- outlier.rank(
+        outlier.statistic = zrange.mean,
+        method = 'zrange.mean'
+        );
+    rank.zrange.median <- outlier.rank(
+        outlier.statistic = zrange.median,
+        method = 'zrange.median'
+        );
+    rank.zrange.trimmean <- outlier.rank(
+        outlier.statistic = zrange.trimmean,
+        method = 'zrange.trimmean'
+        );
+    rank.fraction.kmeans <- outlier.rank(
+        outlier.statistic = fraction.kmeans,
+        method = 'fraction.kmeans'
+        );
+    rank.cosine.similarity <- outlier.rank(
+        outlier.statistic = cosine.similarity,
+        method = 'cosine.similarity'
+        );
+    # Compute the rank product for each transcript.
+    rank.product <- outlier.rank.product(
+        zrange.mean = rank.zrange.mean,
+        zrange.median = rank.zrange.median,
+        zrange.trimmean = rank.zrange.trimmean,
+        fraction.kmeans = rank.fraction.kmeans,
+        cosine.similarity = rank.cosine.similarity
+        );
+    # Calculate the p-value associated with the most abundant sample
+    # in `x`.
+    p.value <- (sum(rank.product[1] >= rank.product[-1]) + 1) / length(rank.product);
+    p.values[most.abundant.sample] <- p.value;
+
+    while (p.value < p.value.threshold) {
+        # Remove the most abundant sample from `x`.
+        x <- x[-index.most.abundant.sample];
+        # Get the name and index of the next most abundant sample.
+        index.most.abundant.sample <- which.max(x);
+        most.abundant.sample <- names(x)[index.most.abundant.sample];
+
+        # Compute quantities for outlier detection in the remaining
+        # samples of the observed transcript.
+        x.zrange.mean <- quantify.outliers(
+            x = x,
+            method = 'mean'
+            );
+        x.zrange.median <- quantify.outliers(
+            x = x,
+            method = 'median'
+            );
+        x.zrange.trimmean <- quantify.outliers(
+            x = x,
+            method = 'mean',
+            trim = 0.05
+            );
+        x.fraction.kmeans <- quantify.outliers(
+            x = x,
+            method = 'kmeans',
+            nstart = kmeans.nstart
+            );
+        # Compute the ranges of the z-score statistics.
+        x.zrange.mean <- zrange(
+            x = x.zrange.mean
+            );
+        x.zrange.median <- zrange(
+            x = x.zrange.median
+            );
+        x.zrange.trimmean <- zrange(
+            x = x.zrange.trimmean
+            );
+        # Compute the k-means fraction.
+        x.fraction.kmeans <- kmeans.fraction(
+            x = x.fraction.kmeans
+            );
+        # Compute the cosine similarity.
+        x.cosine.similarity <- outlier.detection.cosine(
+            x = x,
+            distribution = x.distribution
+            );
+        # Record the outlier statistics for the remaining samples in
+        # `x`.
+        next.entry.name <- paste0(
+            'outlier.statistics.',
+            length(outlier.statistics.list)
+            );
+        outlier.statistics.list[[next.entry.name]] <- c(
+            x.zrange.mean,
+            x.zrange.median,
+            x.zrange.trimmean,
+            x.fraction.kmeans,
+            x.cosine.similarity
+            );
+        names(outlier.statistics.list[[next.entry.name]]) <- c(
+            'zrange.mean',
+            'zrange.median',
+            'zrange.trimmean',
+            'fraction.kmeans',
+            'cosine.similarity'
+            );
+
+        # Combine the recomputed outlier statistics from the remaining
+        # samples of this observed transcript with those of the null
+        # data.
+        zrange.mean <- c(x.zrange.mean, null.zrange.mean);
+        zrange.median <- c(x.zrange.median, null.zrange.median);
+        zrange.trimmean <- c(x.zrange.trimmean, null.zrange.trimmean);
+        fraction.kmeans <- c(x.fraction.kmeans, null.fraction.kmeans);
+        cosine.similarity <- c(x.cosine.similarity, null.cosine.similarity);
+        # Assign ranks within each method.
+        rank.zrange.mean <- outlier.rank(
+            outlier.statistic = zrange.mean,
+            method = 'zrange.mean'
+            );
+        rank.zrange.median <- outlier.rank(
+            outlier.statistic = zrange.median,
+            method = 'zrange.median'
+            );
+        rank.zrange.trimmean <- outlier.rank(
+            outlier.statistic = zrange.trimmean,
+            method = 'zrange.trimmean'
+            );
+        rank.fraction.kmeans <- outlier.rank(
+            outlier.statistic = fraction.kmeans,
+            method = 'fraction.kmeans'
+            );
+        rank.cosine.similarity <- outlier.rank(
+            outlier.statistic = cosine.similarity,
+            method = 'cosine.similarity'
+            );
+        # Compute the rank product for each transcript.
+        rank.product <- outlier.rank.product(
+            zrange.mean = rank.zrange.mean,
+            zrange.median = rank.zrange.median,
+            zrange.trimmean = rank.zrange.trimmean,
+            fraction.kmeans = rank.fraction.kmeans,
+            cosine.similarity = rank.cosine.similarity
+            );
+        # Calculate the p-value associated with the most abundant sample
+        # in `x`.
+        p.value <- (sum(rank.product[1] >= rank.product[-1]) + 1) / length(rank.product);
+        p.values[most.abundant.sample] <- p.value;
+        }
+
+    # After the while loop terminates, all remaining p-values are
+    # greater than `p.value.threshold`.  Replace any missing values or
+    # values greater than `p.value.threshold` in `p.values` with
+    # `p.value.threshold`.
+    p.values[is.na(p.values) | p.values > p.value.threshold] <- p.value.threshold;
+
+    list(
+        p.values = p.values,
+        outlier.statistics.list = outlier.statistics.list
+        );
+    }
