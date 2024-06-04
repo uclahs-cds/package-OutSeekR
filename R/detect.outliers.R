994ccb40f85c00b4d5a043996be6e042fce48bb5
John M Sahrmann <jsahrmann@mednet.ucla.edu>
2024-06-04 12:03:11 -0700

    Create FDR matrix in `detect.outliers()`
    Create a matrix of FDR-adjusted p-values (identical in structure to
    the unadjusted p-values matrix) in `detect.outliers()`.  We build the
    matrix through a somewhat-convoluted process of combining the data
    frames of outlier test results and reshaping the resulting data frame
    from long to wide format, then we fill in the matrix with the FDR
    values.  As with the p-values matrix, entries in the FDR matrix
    corresponding to samples and transcripts for which the outlier test
    was not run (because the p-value associated with another sample
    exceeded `p.value.threshold`) will be `NA`.
    
    Also count the number of outliers based on the FDR-adjusted p-values
    using a new parameter, `fdr.threshold`.  Change the existing outlier
    counts vector to `num.outliers.unadjusted`, with the new counts vector
    being `num.outliers.adjusted`.  Both are included in the return value
    of `detect.outliers()`.
    
    Update the documentation for `detect.outliers()` to reflect additions
    and changes to the return value.

--- R/detect.outliers.R
+++ R/detect.outliers.R
@@ -2,13 +2,16 @@
 #'
 #' @param data A matrix or data frame of FPKM values, organized with transcripts on rows and samples on columns.  Transcript identifiers should be stored as `rownames(data)`.
 #' @param num.null The number of transcripts to generate when simulating from null distributions.
-#' @param p.value.threshold The p-value threshold for the outlier test; default is 0.05.  Once the p-value for a sample exceeds `p.value.threshold`, testing for that transcript ceases, and all samples (including the one that initially exceeded `p.value.threshold`) are assigned p-values equal to `p.value.threshold`.
+#' @param p.value.threshold The p-value threshold for the outlier test; default is 0.05.  Once the p-value for a sample exceeds `p.value.threshold`, testing for that transcript ceases, and all remaining samples will have p-values equal to `NA`.
+#' @param fdr.threshold The false discovery rate (FDR)-adjusted p-value threshold for determining the final count of outliers; default is 0.01.
 #' @param kmeans.nstart The number of random starts when computing k-means fraction; default is 1.  See `?stats::kmeans` for further details.
 #'
 #' @return A list consisting of the following entries:
-#' * `p.values`: a matrix of p-values for the outlier test run on each sample for each transcript in `data`.
-#' * `num.outliers`: a vector giving the number of outliers (specifically, the number of samples for which the outlier test yielded a p-value less than `p.value.threshold`)  for each transcript.
-#' * `outlier.statistics.matrix.list`: a list of length `max(num.outliers) + 1` containing entries `outlier.statistics.matrix.N`, where `N` is between zero and `max(num.outliers)`.  `outlier.statistics.matrix.N` is the matrix of outlier statistics after excluding the `N`th outlier sample, with `outlier.statistics.matrix.0` being for the full data set.  A transcript will only appear in the matrices up to the total number of outliers in that transcript; all transcripts appear in `outlier.statistics.matrix.0`.
+#' * `p.values`: a matrix of unadjusted p-values for the outlier test run on each sample for each transcript in `data`.
+#' * `fdr`: a matrix of FDR-adjusted p-values for the outlier test run on each sample for each transcript in `data`.
+#' * `num.outliers.unadjusted`: a vector giving the number of samples for which the outlier test yielded a p-value less than `p.value.threshold` for each transcript.
+#' * `num.outliers.adjuste`: a vector giving the number of samples for which the outlier test yielded an FDR-adjusted p-value less than `fdr.threshold` for each transcript.
+#' * `outlier.test.results.list`: a list of length `max(num.outliers.unadjusted) + 1` containing entries `roundN`, where `N` is between one and `max(num.outliers.unadjusted) + 1`.  `roundN` is the data frame of results for the outlier test after excluding the (N-1)th outlier sample, with `round1` being for the original data set (i.e., before excluding any outlier samples).  A transcript will only appear in the data frames up to the total number of outliers in that transcript plus one; for example, a transcript with one outlier will appear in `round1` and `round2`.  All transcripts appear in `round1`.
 #' * `distributions`: a numeric vector indicating the optimal distribution for each transcript.  Possible values are 1 (normal), 2 (log-normal), 3 (exponential), and 4 (gamma).
 #'
 #' @export
@@ -16,6 +19,7 @@ detect.outliers <- function(
     data,
     num.null,
     p.value.threshold = 0.05,
+    fdr.threshold = 0.01,
     kmeans.nstart = 1
     ) {
     # Determine which of the normal, log-normal, exponential, or gamma
@@ -268,23 +272,18 @@ detect.outliers <- function(
             )
         );
     rownames(p.values) <- rownames(data);
-    # Get counts of the number of outliers per transcript.
-    num.outliers <- apply(
+    # Get counts of the number of outliers per transcript based on the
+    # unadjusted p-values from the outlier test.
+    num.outliers.unadjusted <- apply(
         X = p.values,
         MARGIN = 1,
         FUN = function(x) sum(x < p.value.threshold, na.rm = TRUE)
         );
-    # Create a list of outlier test results data frames.  The list
-    # will be of length `max(num.outliers) + 1` and will contain
-    # entries `roundN`, where `N` is between one and
-    # `max(num.outliers) + 1`.  `roundN` is the data frame of results
-    # for the outlier test after excluding the `N`th outlier sample,
-    # with `round1` being for the original data set (i.e., before
-    # excluding any outlier samples).  A transcript will only appear
-    # in the data frames up to the total number of outliers in that
-    # transcript plus one; all transcripts appear in `round1`.
+    # Create a list of outlier test results data frames.  (See the
+    # documentation for the return value of this function for details
+    # of its structure.)
     outlier.test.results.list <- list();
-    for (i in 1:(max(num.outliers) + 1)) {
+    for (i in 1:(max(num.outliers.unadjusted) + 1)) {
         next.name <- paste0('round', i);
         outlier.test.results.list[[next.name]] <- list();
         for (j in seq_along(outlier.test.results)) {
@@ -307,8 +306,14 @@ detect.outliers <- function(
             p = outlier.test.results.list[[next.name]]$p.value,
             method = 'fdr'
             );
-        rownames(outlier.test.results.list[[next.name]]) <- names(num.outliers[num.outliers >= (i - 1)]);
+        # Add transcript name to the data frame as the row names and
+        # as a dedicated column.
+        outlier.test.results.list[[next.name]]$transcript <- names(num.outliers.unadjusted[num.outliers.unadjusted >= (i - 1)]);
+        outlier.test.results.list[[next.name]] <- outlier.test.results.list[[next.name]][, c('transcript', colnames(outlier.test.results.list[[next.name]])['transcript' != colnames(outlier.test.results.list[[next.name]])])];
+        rownames(outlier.test.results.list[[next.name]]) <- names(num.outliers.unadjusted[num.outliers.unadjusted >= (i - 1)]);
         }
+    # Assemble a matrix of FDR-adjusted p-values for each transcript
+    # and sample.
     fdr <- matrix(
         data = NA,
         nrow = nrow(data),
@@ -318,32 +323,47 @@ detect.outliers <- function(
             colnames(data)
             )
         );
-    for (i in seq_along(outlier.test.results.list)) {
-        temp <- outlier.test.results.list[[i]];
-        temp <- temp[, c('sample', 'fdr')];
-        temp$transcript <- rownames(temp);
-        fdr.this.round <- stats::reshape(
-            data = temp,
-            direction = 'wide',
-            idvar = 'transcript',
-            timevar = 'sample'
-            );
-        rownames(fdr.this.round) <- fdr.this.round$transcript;
-        fdr.this.round$transcript <- NULL;
-        colnames(fdr.this.round) <- sub(
-            pattern = 'fdr.',
-            replacement = '',
-            x = colnames(fdr.this.round),
-            fixed = TRUE
-            );
-        fdr.this.round <- as.matrix(fdr.this.round);
-        fdr[rownames(fdr.this.round), colnames(fdr.this.round)] <- fdr.this.round;
-        }
+    # Combine the data frames of outlier test results across rounds.
+    # Keep only the transcript names (the rows of the matrix `fdr`),
+    # the sample names (the columns of the matrix `fdr`), and the
+    # FDR-adjusted p-values.
+    all.results <- do.call(
+        what = rbind,
+        args = outlier.test.results.list
+        );
+    all.results <- all.results[, c('transcript', 'sample', 'fdr')];
+    # Reshape from long to wide format.
+    temp.fdr <- stats::reshape(
+        data = all.results,
+        direction = 'wide',
+        idvar = 'transcript',
+        timevar = 'sample'
+        );
+    rownames(temp.fdr) <- temp.fdr$transcript;
+    temp.fdr$transcript <- NULL;
+    # Drop the prefix 'fdr.' from the column names.
+    colnames(temp.fdr) <- sub(
+        pattern = 'fdr.',
+        replacement = '',
+        x = colnames(temp.fdr),
+        fixed = TRUE
+        );
+    temp.fdr <- as.matrix(temp.fdr);
+    # Fill in `fdr` with the FDR-adjusted p-values.
+    fdr[rownames(temp.fdr), colnames(temp.fdr)] <- temp.fdr;
+    # Get counts of the number of outliers per transcript based on the
+    # FDR-adjusted p-values.
+    num.outliers.adjusted <- apply(
+        X = fdr,
+        MARGIN = 1,
+        FUN = function(x) sum(x < fdr.threshold, na.rm = TRUE)
+        );
 
     list(
         p.values = p.values,
         fdr = fdr,
-        num.outliers = num.outliers,
+        num.outliers.unadjusted = num.outliers.unadjusted,
+        num.outliers.adjusted = num.outliers.adjusted,
         outlier.test.results.list = outlier.test.results.list,
         distributions = optimal.distribution.data
         );
