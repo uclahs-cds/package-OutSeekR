0700025dea702b423b63d997c24f9b714192ef3b
John M Sahrmann <jsahrmann@mednet.ucla.edu>
2024-06-10 10:57:22 -0700

    Document example data set for vignette
    Document example data set for vignette in `R/data.R`.  This follows
    the advice of "R Packages" (2e) for documenting exported data sets:
    https://r-pkgs.org/data.html#sec-documenting-data.

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Example data set for outlier testing
+#'
+#' @format
+#' A data frame with 500 rows and 50 columns:
+#' \describe{
+#'   \item{S01, ..., S50}{simulated fragments per kilobase of transcript per million fragments mapped (FPKM) values for sample 1, ..., sample 50}
+#' }
+'outliers'
